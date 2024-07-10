2f996fcd3e39c7e565526a48300562f40f642855
Your Name <you@example.com>
2024-07-10 23:04:11 +0300

    אנימציות

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,115 @@
+// import React, { useRef, useState } from "react";
+
+// import "./App.css";
+// import Carousel from "react-multi-carousel";
+// import "react-multi-carousel/lib/styles.css";
+
+// import HTMLFlipBook from "react-pageflip";
+// import MyBook from "./components/MyBook";
+
+// export default function App() {
+//   const [showBook, setShowBook] = useState(false);
+//   const bookClicked = (event) => {
+//     console.log(event.target.id);
+//     // document.getElementById(event.target.id).style.cssText = "width:300%;rotate:20deg;margin:-20%;margin-left: -30%";
+//     setShowBook(!showBook);
+//   };
+//   // function bookClicked(e) {
+//   //   console.log(e.currentTarget.getAttribute("id"));
+//   //   return(
+//   //     <MyBook />
+//   //   );
+//   // }
+//   return (
+//     <>
+//       <img src="src/assets/frame-books.png" id="top-books" />
+//       <h1 id="header">ספריית השלישות</h1>
+//       {showBook ? <MyBook /> : " "}
+//       {!showBook ? (
+//         <Carousel
+//           additionalTransfrom={0}
+//           // arrows
+//           autoPlaySpeed={3000}
+//           centerMode
+//           containerClass="container"
+//           dotListClass=""
+//           draggable
+//           focusOnSelect={false}
+//           infinite
+//           itemClass=""
+//           keyBoardControl
+//           minimumTouchDrag={80}
+//           pauseOnHover
+//           // renderArrowsWhenDisabled={false}
+//           renderButtonGroupOutside={false}
+//           renderDotsOutside={false}
+//           responsive={{
+//             desktop: {
+//               breakpoint: {
+//                 max: 3000,
+//                 min: 1024,
+//               },
+//               items: 3,
+//               partialVisibilityGutter: 40,
+//             },
+//             mobile: {
+//               breakpoint: {
+//                 max: 464,
+//                 min: 0,
+//               },
+//               items: 1,
+//               partialVisibilityGutter: 30,
+//             },
+//             tablet: {
+//               breakpoint: {
+//                 max: 1024,
+//                 min: 464,
+//               },
+//               items: 2,
+//               partialVisibilityGutter: 30,
+//             },
+//           }}
+//           rewind={false}
+//           rewindWithAnimation={false}
+//           rtl={false}
+//           shouldResetAutoplay
+//           showDots={false}
+//           sliderClass=""
+//           slidesToSlide={1}
+//           swipeable
+//           // className="react-multiple-carousel__arrow--right react-multiple-carousel__arrow--left"
+//         >
+//           <img
+//             onClick={bookClicked}
+//             id="book1"
+//             className="books"
+//             src="src\assets\tulip-img.png"
+//           />
+//           <img className="books" src="src\assets\brown-img.png" />
+//           <img className="books" src="src\assets\blue-img.png" />
+//           <img className="books" src="src\assets\bezh-img.png" />
+//           <img className="books" src="src\assets\dark-img.png" />
+//         </Carousel>
+//       ) : (
+//         " "
+//       )}
+
+//       <img src="src/assets/frame-books.png" id="bottom-books" />
+//     </>
+//   );
+// }
 import React, { useRef, useState } from "react";
 
 import "./App.css";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 
-import HTMLFlipBook from "react-pageflip";
 import MyBook from "./components/MyBook";
 
 export default function App() {
   const [showBook, setShowBook] = useState(false);
   const bookClicked = (event) => {
     console.log(event.target.id);
-    // document.getElementById(event.target.id).style.cssText = "width:300%;rotate:20deg;margin:-20%;margin-left: -30%";
     setShowBook(!showBook);
   };
   // function bookClicked(e) {
@@ -22,91 +120,76 @@ export default function App() {
   // }
   return (
     <>
-
-      <img src="src/assets/frame-books.png" id="top-books"/>
+      <img src="src/assets/frame-books.png" id="top-books" />
       <h1 id="header">ספריית השלישות</h1>
-      {showBook?<MyBook />:" "}
-  {!showBook?<Carousel 
-        additionalTransfrom={0}
-        // arrows
-        autoPlaySpeed={3000}
-        centerMode
-        className=""
-        containerClass="container"
-        dotListClass=""
-        draggable
-        focusOnSelect={false}
-        infinite
-        itemClass=""
-        keyBoardControl
-        minimumTouchDrag={80}
-        pauseOnHover
-        // renderArrowsWhenDisabled={false}
-        renderButtonGroupOutside={false}
-        renderDotsOutside={false}
-        responsive={{
-          desktop: {
-            breakpoint: {
-              max: 3000,
-              min: 1024,
+      {!showBook ? (
+        <Carousel
+          additionalTransfrom={0}
+          autoPlaySpeed={3000}
+          centerMode
+          containerClass="container"
+          dotListClass=""
+          draggable
+          focusOnSelect={false}
+          infinite
+          itemClass=""
+          keyBoardControl
+          minimumTouchDrag={80}
+          pauseOnHover
+          renderButtonGroupOutside={false}
+          renderDotsOutside={false}
+          responsive={{
+            desktop: {
+              breakpoint: {
+                max: 3000,
+                min: 1024,
+              },
+              items: 3,
+              partialVisibilityGutter: 40,
             },
-            items: 3,
-            partialVisibilityGutter: 40,
-          },
-          mobile: {
-            breakpoint: {
-              max: 464,
-              min: 0,
+            mobile: {
+              breakpoint: {
+                max: 464,
+                min: 0,
+              },
+              items: 1,
+              partialVisibilityGutter: 30,
             },
-            items: 1,
-            partialVisibilityGutter: 30,
-          },
-          tablet: {
-            breakpoint: {
-              max: 1024,
-              min: 464,
+            tablet: {
+              breakpoint: {
+                max: 1024,
+                min: 464,
+              },
+              items: 2,
+              partialVisibilityGutter: 30,
             },
-            items: 2,
-            partialVisibilityGutter: 30,
-          },
-        }}
-        rewind={false}
-        rewindWithAnimation={false}
-        rtl={false}
-        shouldResetAutoplay
-        showDots={false}
-        sliderClass=""
-        slidesToSlide={1}
-        swipeable
-        class = "react-multiple-carousel__arrow"
-      >
-        <img
-          onClick={bookClicked}
-          id="book1"
-          className="books"
-          src="src\assets\tulip-img.png"
-        />
-        <img
-          class="books"
-          src="src\assets\brown-img.png"
-        />
-        <img
-          class="books"
-          src="src\assets\blue-img.png"
-        />
-        <img
-          class="books"
-          src="src\assets\bezh-img.png"
-        />
-        <img
-          class="books"
-          src="src\assets\dark-img.png"
-        />
-      </Carousel>:" "}
-      
+          }}
+          rewind={false}
+          rewindWithAnimation={false}
+          rtl={false}
+          shouldResetAutoplay
+          showDots={false}
+          sliderClass=""
+          slidesToSlide={1}
+          swipeable
+        >
+          <img
+            onClick={bookClicked}
+            id="book1"
+            className="books"
+            src="src\assets\tulip-img.png"
+          />
+          <img className="books" src="src\assets\brown-img.png" />
+          <img className="books" src="src\assets\blue-img.png" />
+          <img className="books" src="src\assets\bezh-img.png" />
+          <img className="books" src="src\assets\dark-img.png" />
+        </Carousel>
+      ) : (
+        " "
+      )}
 
-      <img src="src/assets/frame-books.png" id="bottom-books"/>
+      <img src="src/assets/frame-books.png" id="bottom-books" />
+      {showBook ? <MyBook /> : ""}
     </>
-    
   );
 }
--- src/components/MyBook.jsx
+++ src/components/MyBook.jsx
@@ -1,7 +1,169 @@
+// import React, { useRef, useState, useEffect } from "react";
+// import HTMLFlipBook from "react-pageflip";
+// import "./MyBook.css";
+// import "intro.js/introjs.css";
+
+// const PageCover = React.forwardRef((props, ref) => {
+//   return (
+//     <div className="cover" ref={ref} data-density="hard">
+//       <div>
+//         <h2>{props.children}</h2>
+//       </div>
+//     </div>
+//   );
+// });
+
+// const Page = React.forwardRef((props, ref) => {
+//   console.log(props.menu);
+//   // const list=["חדר כושר",'חד"א',"בריכה","נהלי יחידה",'שק"ם','יוהל"ם',"טלפונים חשובים",'ת"ש',"היסעים","תורנויות","רפואה","שלישות"];
+  
+//   return (
+//     <div className="page" ref={ref}>
+//       <h1>{props.header}</h1>
+//       {/* <h1>{props.menu}</h1> */}
+//       <dl className="dl1">
+//         {props.menu.map((option, index) => (
+//           <dt key={index}>{option}</dt>
+//         ))}
+//       </dl>
+//       <dl className="dl2">
+//         {props.menu.map((option, index) => (
+//           <dt key={index}>{index}</dt>
+//         ))}
+//       </dl>
+//       {/* {menu.map(name=> (
+//           <ul>
+//             {name}
+//           </ul>
+//       ))} */}
+//       {/* <p>{props.children}</p> */}
+//       {/* {for(let i=0;i<12;i++) 
+//         <ul>{props.menu}</ul>
+//       } */}
+//       {/* <p>{props.number}</p>
+//         <ul>
+//           {props.menu.map((option) => (
+//             <li key={option.id}>{option.brand}</li>
+//           ))}
+//         </ul> */}
+
+//       {/* <p>{cars.map((car) => <Car key={car.id} brand={car.brand} />)}</p> */}
+//       {/* <ul>
+//       {cars.map((car) => <Car key={car.id} brand={car.brand} />)}
+//       </ul> */}
+//       {/* for i in range(11)
+//         <li>{ props.menu }</li>
+//       endform   */}
+//       {/* <p></p> */}
+//     </div>
+//   );
+// });
+
+// export default function MyBook(props) {
+//   const book = useRef();
+//   // this.pageFlip.getPageFlip().flipNext();
+//   const [inputText, setInputElement] = useState("");
+//   const [menu, setMenu] = useState([
+//     "חדר כושר",
+//     'חד"א',
+//     "בריכה",
+//     "נהלי יחידה",
+//     'שק"ם',
+//     'יוהל"ם',
+//     "טלפונים חשובים",
+//     'ת"ש',
+//     "היסעים",
+//     "תורנויות",
+//     "רפואה",
+//     "שלישות",
+//   ]);
+//   const [text, setText] = useState("ここに表示されます。");
+//   const printText = () => {
+//     setText(inputText);
+//     setInputElement("");
+//   };
+//   const cars = [
+//     { id: 1, brand: "שלישות" },
+//     { id: 2, brand: "רפואה" },
+//     { id: 3, brand: "תורנויות" },
+//     { id: 4, brand: "היסעים" },
+//     { id: 5, brand: 'ת"ש' },
+//     { id: 6, brand: "טלפונים חשובים" },
+//     { id: 7, brand: 'יוהל"ם' },
+//     { id: 8, brand: 'שק"ם' },
+//     { id: 9, brand: "נהלי יחידה" },
+//     { id: 10, brand: "בריכה" },
+//     { id: 11, brand: "חדר אוכל" },
+//     { id: 12, brand: "חדר כושר" },
+//   ];
+//   //const menu=["חדר כושר",'חד"א',"בריכה","נהלי יחידה",'שק"ם','יוהל"ם',"טלפונים חשובים",'ת"ש',"היסעים","תורנויות","רפואה","שלישות"];
+//   // useEffect(() => {
+//   //   document.getElementById("book-album").classList.add("animations");
+//   // });
+//   return (
+//     <>
+//       {/* <button onClick={() => book.current.pageFlip().flip(3)}>Next page</button> */}
+//       <div>
+//         <HTMLFlipBook
+//           width={550}
+//           height={650}
+//           minWidth={315}
+//           maxWidth={1000}
+//           minHeight={420}
+//           maxHeight={1350}
+//           showCover={true}
+//           flippingTime={1000}
+//           style={{ margin: "0 auto" }}
+//           maxShadowOpacity={0.5}
+//           className="album-web"
+//           // id="book-album"
+//           drawShadow={true}
+//           ref={book}
+//         >
+//           <PageCover>
+//             <b id="header-book">קליטת חייל חדש</b>
+//           </PageCover>
+//           <Page header="תוכן עניינים" number="1" menu={menu}>
+//             <hr></hr>
+//             <p contentEditable="true">ここは編集可能です</p>
+//           </Page>
+//           {/* <Page number="2">
+//             <hr></hr>
+//             <p>{text}</p>
+//           </Page>
+//           <Page number="3">
+//             <hr></hr>
+//           </Page>
+//           <Page number="4">
+//             <hr></hr>
+//           </Page> */}
+//           <PageCover className="color-cover">see you</PageCover>
+//         </HTMLFlipBook>
+//         {/* <ul>
+//               {cars.map((car) => <Page key={car.id} brand={car.brand} />)}
+//             </ul> */}
+//         <br></br>
+//         <br></br>
+//         {/* <div className="formContainer">
+//           <input
+//             className="form-control"
+//             value={inputText}
+//             onChange={(e) => setInputElement(e.target.value)}
+//             type="text"
+//             placeholder="入力してボタンを押してください。"
+//           />
+//           <button className="btn" onClick={printText}>
+//             表示する
+//           </button>
+//         </div> */}
+//       </div>
+//     </>
+//   );
+// }
 import React, { useRef, useState } from "react";
 import HTMLFlipBook from "react-pageflip";
 import "./MyBook.css";
-import 'intro.js/introjs.css';
+import "intro.js/introjs.css";
 
 const PageCover = React.forwardRef((props, ref) => {
   return (
@@ -20,17 +182,15 @@ const Page = React.forwardRef((props, ref) => {
     <div className="page" ref={ref}>
       <h1>{props.header}</h1>
       {/* <h1>{props.menu}</h1> */}
-       <dl className="dl1">
-          {props.menu.map((option, index) => (
-            <dt key={index}>{option}</dt>
-            
-          ))}
+      <dl className="dl1">
+        {props.menu.map((option, index) => (
+          <dt key={index}>{option}</dt>
+        ))}
       </dl>
       <dl className="dl2">
-          {props.menu.map((option, index) => (
-            <dt key={index}>{index}</dt>
-
-          ))}
+        {props.menu.map((option, index) => (
+          <dt key={index}>{index}</dt>
+        ))}
       </dl>
       {/* {menu.map(name=> (
           <ul>
@@ -47,7 +207,7 @@ const Page = React.forwardRef((props, ref) => {
             <li key={option.id}>{option.brand}</li>
           ))}
         </ul> */}
-     
+
       {/* <p>{cars.map((car) => <Car key={car.id} brand={car.brand} />)}</p> */}
       {/* <ul>
       {cars.map((car) => <Car key={car.id} brand={car.brand} />)}
@@ -56,7 +216,6 @@ const Page = React.forwardRef((props, ref) => {
         <li>{ props.menu }</li>
       endform   */}
       {/* <p></p> */}
-     
     </div>
   );
 });
@@ -65,32 +224,45 @@ export default function MyBook(props) {
   const book = useRef();
   // this.pageFlip.getPageFlip().flipNext();
   const [inputText, setInputElement] = useState("");
-  const [menu, setMenu] = useState(["חדר כושר",'חד"א',"בריכה","נהלי יחידה",'שק"ם','יוהל"ם',"טלפונים חשובים",'ת"ש',"היסעים","תורנויות","רפואה","שלישות"]);
+  const [menu, setMenu] = useState([
+    "חדר כושר",
+    'חד"א',
+    "בריכה",
+    "נהלי יחידה",
+    'שק"ם',
+    'יוהל"ם',
+    "טלפונים חשובים",
+    'ת"ש',
+    "היסעים",
+    "תורנויות",
+    "רפואה",
+    "שלישות",
+  ]);
   const [text, setText] = useState("ここに表示されます。");
   const printText = () => {
     setText(inputText);
     setInputElement("");
   };
   const cars = [
-    {id: 1, brand: 'שלישות'},
-    {id: 2, brand: 'רפואה'},
-    {id: 3, brand: 'תורנויות'},
-    {id: 4, brand: 'היסעים'},
-    {id: 5, brand: 'ת"ש'},
-    {id: 6, brand: 'טלפונים חשובים'},
-    {id: 7, brand: 'יוהל"ם'},
-    {id: 8, brand: 'שק"ם'},
-    {id: 9, brand: 'נהלי יחידה'},
-    {id: 10, brand: 'בריכה'},
-    {id: 11, brand: 'חדר אוכל'},
-    {id: 12, brand: 'חדר כושר'}
+    { id: 1, brand: "שלישות" },
+    { id: 2, brand: "רפואה" },
+    { id: 3, brand: "תורנויות" },
+    { id: 4, brand: "היסעים" },
+    { id: 5, brand: 'ת"ש' },
+    { id: 6, brand: "טלפונים חשובים" },
+    { id: 7, brand: 'יוהל"ם' },
+    { id: 8, brand: 'שק"ם' },
+    { id: 9, brand: "נהלי יחידה" },
+    { id: 10, brand: "בריכה" },
+    { id: 11, brand: "חדר אוכל" },
+    { id: 12, brand: "חדר כושר" },
   ];
+
   //const menu=["חדר כושר",'חד"א',"בריכה","נהלי יחידה",'שק"ם','יוהל"ם',"טלפונים חשובים",'ת"ש',"היסעים","תורנויות","רפואה","שלישות"];
   return (
     <>
-     {/* <button onClick={() => book.current.pageFlip().flip(3)}>Next page</button> */}
-      <div>
-     
+      {/* <button onClick={() => book.current.pageFlip().flip(3)}>Next page</button> */}
+      <div className="book-container">
         <HTMLFlipBook
           width={550}
           height={650}
@@ -102,13 +274,14 @@ export default function MyBook(props) {
           flippingTime={1000}
           style={{ margin: "0 auto" }}
           maxShadowOpacity={0.5}
-          className="album-web"
           drawShadow={true}
           ref={book}
+          className={"open-book"}
         >
-        
-          <PageCover style={{color: "pink"}} className="color-cover"><b id="header-book">קליטת חייל חדש</b></PageCover>
-          <Page header = "תוכן עניינים" number="1" menu={menu}>
+          <PageCover style={{ color: "pink" }} className="color-cover">
+            <b id="header-book">קליטת חייל חדש</b>
+          </PageCover>
+          <Page header="תוכן עניינים" number="1" menu={menu}>
             <hr></hr>
             <p contentEditable="true">ここは編集可能です</p>
           </Page>
@@ -131,17 +304,16 @@ export default function MyBook(props) {
         <br></br>
         {/* <div className="formContainer">
           <input
-            class="form-control"
+            className="form-control"
             value={inputText}
             onChange={(e) => setInputElement(e.target.value)}
             type="text"
             placeholder="入力してボタンを押してください。"
           />
-          <button class="btn" onClick={printText}>
+          <button className="btn" onClick={printText}>
             表示する
           </button>
         </div> */}
-        
       </div>
     </>
   );
